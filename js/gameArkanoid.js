e65e06cdd2b33f7c3e2548aba113e4c17c4a5911
puzatkin-ivan <puzatkin1998@gmail.com>
2017-06-30 10:45:48 +0300

    Внес изменения по замечаниям

--- js/gameArkanoid.js
+++ js/gameArkanoid.js
@@ -2,17 +2,18 @@
 
 const WIDTH_CANVAS = 1500;
 const HEIGHT_CANVAS = 600;
-const DIRECTION_UP = "direction_up";
-const DIRECTION_LEFT = "direction_left";
-const DIRECTION_RIGHT = "direction_right";
-const DIRECTION_DOWN = "direction_down";
-const WIDTH_BORDER_GAMES_FIELD = 20;
+const WIDTH_BORDER_GAMES_FIELD = 25;
+const DIRECTION = {
+  UP: "direction_up",
+  LEFT: "direction_left",
+  RIGHT:"direction_right",
+  DOWN: "direction_down",
+};
+const LENGTH_PLATFORM = 100;
 const INITIAL_POSITION_BALL_X = WIDTH_CANVAS / 2;
 const INITIAL_POSITION_BALL_Y = 505;
-const LENGTH_PLATFORM = 100;
 const INITIAL_POSITION_PLATFORM_X = INITIAL_POSITION_BALL_X - LENGTH_PLATFORM / 2;
 const INITIAL_POSITION_PLATFORM_Y = 515;
-const TIME_BETWEEN_FRAMES = 1000 / 60;
 
 class Ball {
   constructor(x, y) {
@@ -49,12 +50,12 @@ class Ball {
     this.directionY = direction;
   }
 
-  move(deltaTime) { //сделать првильную физику
+  move(deltaTime) {
     const SPEED = 200;
     const DELTA_MOVE = SPEED * deltaTime / 1000;
     if (this.isFlying) {
-      this.x += (this.directionX === DIRECTION_RIGHT) ? DELTA_MOVE : -DELTA_MOVE;
-      this.y += (this.directionY === DIRECTION_DOWN) ? DELTA_MOVE : -DELTA_MOVE;
+      this.x += (this.directionX === DIRECTION.RIGHT) ? DELTA_MOVE : -DELTA_MOVE;
+      this.y += (this.directionY === DIRECTION.DOWN) ? DELTA_MOVE : -DELTA_MOVE;
     }
   }
 }
@@ -88,12 +89,12 @@ class Platform {
     const DELTA_MOVE = STEP * deltaTime / 1000;
     const MAX_POSITION = WIDTH_CANVAS - LENGTH_PLATFORM - WIDTH_BORDER_GAMES_FIELD;
     const MIN_POSITION = WIDTH_BORDER_GAMES_FIELD;
-    if (this.directionPlatform === DIRECTION_RIGHT) {
+    if (this.directionPlatform === DIRECTION.RIGHT) {
       this.x += DELTA_MOVE;
       if (this.x > MAX_POSITION) {
         this.x = MAX_POSITION;
       }
-    } else if (this.directionPlatform === DIRECTION_LEFT) {
+    } else if (this.directionPlatform === DIRECTION.LEFT) {
       this.x -= DELTA_MOVE;
       if (this.x < MIN_POSITION) {
         this.x = MIN_POSITION;
@@ -124,80 +125,114 @@ class Brick {
     context.closePath();
   }
 }
+Brick.WIDTH = 50;
+Brick.HEIGHT = 20;
 
-class ConrolLevelGame{
-  constructor() {
-  }
+const gameContext = {
+  ball: new Ball(INITIAL_POSITION_BALL_X, INITIAL_POSITION_BALL_Y),
 
-  transitionNextLevel(bricks) {
-    for (const brick of bricks) {
-      if (!brick.isBroken) {
-        this.isTransition = false;
-        break;
-      } else {
-        this.isTransition = true;
-      }
-    }
-  }
-}
+  platform: new Platform(INITIAL_POSITION_PLATFORM_X, INITIAL_POSITION_PLATFORM_Y),
 
-function gameStart() {
-  const CANVAS = document.getElementById("canvas");
-  const context = CANVAS.getContext("2d");
-
-  CANVAS.width = WIDTH_CANVAS;
-  CANVAS.height = HEIGHT_CANVAS;
-
-  const gameContext = {
-    ball: new Ball(INITIAL_POSITION_BALL_X, INITIAL_POSITION_BALL_Y),
-
-    platform: new Platform(INITIAL_POSITION_PLATFORM_X, INITIAL_POSITION_PLATFORM_Y),
-
-    bricks: [
-      new Brick(20, 20, "#0d1dab"),
-      new Brick(70, 40, "#3fab12"),
-      new Brick(120, 60, "#ab2c33"),
-      new Brick(170, 80, "#fffa82"),
-      new Brick(220, 100, "#3fab12"),
-      new Brick(270, 120, "#fffa82"),
-      new Brick(320, 140, "#0d1dab"),
-      new Brick(370, 160, "#093714"),
-      new Brick(420, 180, "#aba326"),
-      new Brick(470, 200, "#ab2a22"),
-      new Brick(70, 20, "#fffa82"),
-      new Brick(120, 20, "#ab2c33"),
-      new Brick(120, 40, "#fffa82"),
-      new Brick(170, 20, "#3fab12"),
-      new Brick(170,  40, "#fffa82"),
-      new Brick(170, 60, "#0d1dab"),
-      new Brick(220, 60, "#093714"),
-      new Brick(220, 80, "#aba326"),
-      new Brick(220, 20, "#ab2a22"),
-      new Brick(220, 40, "#aba326"),
-      new Brick(270, 20, "#0d1dab"),
-      new Brick(270, 40, "#3fab12"),
-      new Brick(270, 60, "#ab2c33"),
-      new Brick(270, 80, "#fffa82"),
-      new Brick(270, 100, "#3fab12"),
-      new Brick(270, 120, "#fffa82"),
-      new Brick(320, 20, "#0d1dab"),
-      new Brick(320, 40, "#093714"),
-      new Brick(320, 60, "#aba326"),
-      new Brick(320, 80, "#ab2a22"),
-      new Brick(320, 100, "#fffa82"),
-      new Brick(320, 120, "#ab2c33"),
-      new Brick(370, 20, "#0d1dab"),
-      new Brick(370, 40, "#093714"),
-      new Brick(370, 60, "#aba326"),
-      new Brick(370, 80, "#ab2a22"),
-      new Brick(370, 100, "#fffa82"),
-      new Brick(370, 120, "#ab2c33"),
-      new Brick(370, 140, "#ab2c33"),
-
-
+  bricks: {
+    level1: [
+      new Brick(725, 250, "#3fab12"),
+      new Brick(700, 230, "#3fab12"),
+      new Brick(750, 230, "#3fab12"),
+      new Brick(675, 210, "#3fab12"),
+      new Brick(725, 210, "#3fab12"),
+      new Brick(775, 210, "#3fab12"),
+      new Brick(650, 190, "#3fab12"),
+      new Brick(700, 190, "#3fab12"),
+      new Brick(750, 190, "#3fab12"),
+      new Brick(800, 190, "#3fab12"),
+      new Brick(625, 170, "#3fab12"),
+      new Brick(675, 170, "#3fab12"),
+      new Brick(725, 170, "#3fab12"),
+      new Brick(775, 170, "#3fab12"),
+      new Brick(825, 170, "#3fab12"),
+      new Brick(600, 150, "#3fab12"),
+      new Brick(650, 150, "#3fab12"),
+      new Brick(700, 150, "#3fab12"),
+      new Brick(750, 150, "#3fab12"),
+      new Brick(800, 150, "#3fab12"),
+      new Brick(850, 150, "#3fab12"),
+      new Brick(575, 130, "#3fab12"),
+      new Brick(625, 130, "#3fab12"),
+      new Brick(675, 130, "#3fab12"),
+      new Brick(725, 130, "#3fab12"),
+      new Brick(775, 130, "#3fab12"),
+      new Brick(825, 130, "#3fab12"),
+      new Brick(875, 130, "#3fab12"),
+      new Brick(550, 110, "#3fab12"),
+      new Brick(600, 110, "#3fab12"),
+      new Brick(650, 110, "#3fab12"),
+      new Brick(700, 110, "#3fab12"),
+      new Brick(750, 110, "#3fab12"),
+      new Brick(800, 110, "#3fab12"),
+      new Brick(850, 110, "#3fab12"),
+      new Brick(900, 110, "#3fab12"),
+      new Brick(525, 90, "#3fab12"),
+      new Brick(575, 90, "#3fab12"),
+      new Brick(625, 90, "#3fab12"),
+      new Brick(675, 90, "#3fab12"),
+      new Brick(725, 90, "#3fab12"),
+      new Brick(775, 90, "#3fab12"),
+      new Brick(825, 90, "#3fab12"),
+      new Brick(875, 90, "#3fab12"),
+      new Brick(925, 90, "#3fab12"),
+      new Brick(25, 90, "#3fab12"),
+      new Brick(25, 110, "#3fab12"),
+      new Brick(25, 130, "#3fab12"),
+      new Brick(25, 150, "#3fab12"),
+      new Brick(25, 170, "#3fab12"),
+      new Brick(25, 190, "#3fab12"),
+      new Brick(25, 210, "#3fab12"),
+      new Brick(25, 230, "#3fab12"),
+      new Brick(25, 250, "#3fab12"),
+      new Brick(75, 230, "#3fab12"),
+      new Brick(75, 210, "#3fab12"),
+      new Brick(75, 190, "#3fab12"),
+      new Brick(75, 170, "#3fab12"),
+      new Brick(75, 150, "#3fab12"),
+      new Brick(75, 130, "#3fab12"),
+      new Brick(75, 110, "#3fab12"),
+      new Brick(125, 130, "#3fab12"),
+      new Brick(125, 150, "#3fab12"),
+      new Brick(125, 170, "#3fab12"),
+      new Brick(125, 190, "#3fab12"),
+      new Brick(125, 210, "#3fab12"),
+      new Brick(175, 190, "#3fab12"),
+      new Brick(175, 170, "#3fab12"),
+      new Brick(175, 150, "#3fab12"),
+      new Brick(225, 170, "#3fab12"),
+      new Brick(1425, 90, "#3fab12"),
+      new Brick(1425, 110, "#3fab12"),
+      new Brick(1425, 130, "#3fab12"),
+      new Brick(1425, 150, "#3fab12"),
+      new Brick(1425, 170, "#3fab12"),
+      new Brick(1425, 190, "#3fab12"),
+      new Brick(1425, 210, "#3fab12"),
+      new Brick(1425, 230, "#3fab12"),
+      new Brick(1425, 250, "#3fab12"),
+      new Brick(1375, 230, "#3fab12"),
+      new Brick(1375, 210, "#3fab12"),
+      new Brick(1375, 190, "#3fab12"),
+      new Brick(1375, 170, "#3fab12"),
+      new Brick(1375, 150, "#3fab12"),
+      new Brick(1375, 130, "#3fab12"),
+      new Brick(1375, 110, "#3fab12"),
+      new Brick(1325, 130, "#3fab12"),
+      new Brick(1325, 150, "#3fab12"),
+      new Brick(1325, 170, "#3fab12"),
+      new Brick(1325, 190, "#3fab12"),
+      new Brick(1325, 210, "#3fab12"),
+      new Brick(1275, 190, "#3fab12"),
+      new Brick(1275, 170, "#3fab12"),
+      new Brick(1275, 150, "#3fab12"),
+      new Brick(1225, 170, "#3fab12"),
     ],
 
-    bricksLevel2: [
+    level2: [
       new Brick(700, 120, "#0d1dab"),
       new Brick(750, 120, "#3fab12"),
       new Brick(700, 140, "#ab2c33"),
@@ -206,88 +241,120 @@ function gameStart() {
       new Brick(650, 160, "#fffa82"),
       new Brick(700, 180, "#0d1dab"),
     ],
+  },
 
-    controlGame: new ConrolLevelGame(),
-  };
-
-  Brick.WIDTH = 50;
-  Brick.HEIGHT = 20;
-
-  window.addEventListener("keydown", (key) => {keyControl(key, true)});
-  window.addEventListener("keyup", (key) => {keyControl(key, false)});
-
-  const bunchKey = {
+  keyMap: {
     KEY_LEFT: false,
     KEY_RIGHT: false,
     KEY_SPACE: false,
-  };
+  },
+
+  lastTimeFrame: Date.now(),
+  currentLevel: 1,
+};
+
+function processDraw(canvasContext, gameContext) {
+  canvasContext.clearRect(0, 0, WIDTH_CANVAS, HEIGHT_CANVAS);
+  drawBorderGamesField(canvasContext);
+  gameContext.ball.draw(canvasContext);
+  gameContext.platform.draw(canvasContext);
+  for (const brick of gameContext.currentLevel) {
+    if (!brick.isBroken){
+      brick.draw(canvasContext);
+    }
+  }
+}
+
+function processGamePhysics(gameContext, deltaTime) {
+  collisionBallAndBorder(gameContext.ball, gameContext.platform);
+  collisionBallAndPlatform(gameContext.ball, gameContext.platform);
+  for (const brick of gameContext.currentLevel) {
+    collisionBallAndBrick(gameContext.ball, brick);
+  }
+
+  gameContext.ball.move(deltaTime);
+  gameContext.platform.move(gameContext.ball, deltaTime);
+}
+
+function isLevelOver(gameContext) {
+  for (const brick of gameContext.currentLevel) {
+    if (!brick.isBroken) {
+      return false
+    }
+  }
+  return true;
+}
+
+function processTransitionNextLevel(gameContext) {
+  gameContext.ball.x = INITIAL_POSITION_BALL_X;
+  gameContext.ball.y = INITIAL_POSITION_BALL_Y;
+  gameContext.ball.isFlying = false;
+  gameContext.platform.x = INITIAL_POSITION_BALL_X;
+  gameContext.platform.y = INITIAL_POSITION_BALL_Y;
+  gameContext.currentLevel += 1;
+}
+
+const gameLoop = function(canvasContext, gameContext) {
+
+  if (gameContext.keyMap.KEY_LEFT) {
+    gameContext.platform.directionPlatform = DIRECTION.LEFT;
+  } else if (gameContext.keyMap.KEY_RIGHT) {
+    gameContext.platform.directionPlatform = DIRECTION.RIGHT;
+  }
+  if (gameContext.keyMap.KEY_SPACE) {
+    if (!gameContext.ball.isFlying) {
+      gameContext.ball.directionX = DIRECTION.LEFT;
+      gameContext.ball.directionY = DIRECTION.UP;
+    }
+    gameContext.ball.isFlying = true;
+  }
+
+  let currentTimeFrame = Date.now();
+  let deltaTime = currentTimeFrame - gameContext.lastTimeFrame;
+  gameContext.lastTimeFrame = currentTimeFrame;
+
+  processGamePhysics(gameContext, deltaTime);
+  if (isLevelOver) {
+    processTransitionNextLevel(gameContext);
+  }
+  processDraw(canvasContext, gameContext);
+  requestAnimationFrame(function() {
+    gameLoop(canvasContext, gameContext);
+  });
+};
+
+
+function gameStart(gameContext) {
+  const CANVAS = document.getElementById("canvas");
+  const context = CANVAS.getContext("2d");
+
+  CANVAS.width = WIDTH_CANVAS;
+  CANVAS.height = HEIGHT_CANVAS;
 
   const keyControl = function(key, isPressed) {
     const definitionCodeKey = key.code;
 
     switch(definitionCodeKey) {
       case "ArrowRight":
-        bunchKey.KEY_RIGHT = isPressed;
-       break;
+        gameContext.keyMap.KEY_RIGHT = isPressed;
+        break;
       case "ArrowLeft":
-        bunchKey.KEY_LEFT = isPressed;
-       break;
+        gameContext.keyMap.KEY_LEFT = isPressed;
+        break;
       case "Space":
-        bunchKey.KEY_SPACE = isPressed;
-      break;
+        gameContext.keyMap.KEY_SPACE = isPressed;
+        break;
     }
   };
 
-  let lastTimeFrame = Date.now();
-  const gameLoop = function(canvasContext, gameContext) {
-    context.clearRect(0, 0, WIDTH_CANVAS, HEIGHT_CANVAS);
+  window.addEventListener("keydown", (key) => {keyControl(key, true)});
+  window.addEventListener("keyup", (key) => {keyControl(key, false)});
 
-    if (bunchKey.KEY_LEFT) {
-      gameContext.platform.directionPlatform = DIRECTION_LEFT;
-    } else if (bunchKey.KEY_RIGHT) {
-      gameContext.platform.directionPlatform = DIRECTION_RIGHT;
-    }
-    if (bunchKey.KEY_SPACE) {
-      gameContext.ball.isFlying = true;
-      gameContext.ball.directionX = DIRECTION_LEFT;
-      gameContext.ball.directionY = DIRECTION_UP;
-    }
-
-    let currentTimeFrame = Date.now();
-    let deltaTime = currentTimeFrame - lastTimeFrame;
-    if (deltaTime > TIME_BETWEEN_FRAMES) {
-      lastTimeFrame = currentTimeFrame;
-    }
-    gameContext.ball.move(deltaTime);
-    gameContext.platform.move(gameContext.ball, deltaTime);
-    collisionBallAndBorder(gameContext.ball, gameContext.platform);
-    collisionBallAndPlatform(gameContext.ball, gameContext.platform);
-    drawBorderGamesField(canvasContext);
-    gameContext.controlGame.transitionNextLevel(gameContext.bricks);
-    for (const brick of gameContext.bricks) {
-      collisionBallAndBrick(gameContext.ball, brick);
-    }
-    if (gameContext.controlGame.isTransition) {
-      gameContext.bricks = gameContext.bricksLevel2;
-    }
-
-    gameContext.ball.draw(canvasContext);
-    gameContext.platform.draw(canvasContext);
-    for (const brick of gameContext.bricks) {
-      if (!brick.isBroken){
-        brick.draw(canvasContext);
-      }
-    }
-
-    requestAnimationFrame(function() {
-      gameLoop(canvasContext, gameContext);
-    });
-  };
-
+  gameContext.currentLevel = gameContext.bricks.level1;
   gameLoop(context, gameContext);
 }
 
-gameStart();
+gameStart(gameContext);
 
 function drawBorderGamesField(context) {
   const x = 0;
@@ -312,10 +379,10 @@ function collisionBallAndBorder(ball, platform) {
   const ballRight = ball.x + ball.radius;
 
   if (ballLeft < ball.radius + WIDTH_BORDER_GAMES_FIELD) {
-    ball.setDirectionX(DIRECTION_RIGHT);
+    ball.setDirectionX(DIRECTION.RIGHT);
     ball.x = ball.radius + WIDTH_BORDER_GAMES_FIELD;
   } else if (ballRight >= WIDTH_CANVAS - WIDTH_BORDER_GAMES_FIELD) {
-    ball.setDirectionX(DIRECTION_LEFT);
+    ball.setDirectionX(DIRECTION.LEFT);
     ball.x = WIDTH_CANVAS - ball.radius - WIDTH_BORDER_GAMES_FIELD;
   }
 
@@ -323,11 +390,11 @@ function collisionBallAndBorder(ball, platform) {
   const ballDown = ball.y + ball.radius - WIDTH_BORDER_GAMES_FIELD;
 
   if (ballUp < ball.radius + WIDTH_BORDER_GAMES_FIELD) {
-    ball.setDirectionY(DIRECTION_DOWN);
+    ball.setDirectionY(DIRECTION.DOWN);
     ball.y = ball.radius + WIDTH_BORDER_GAMES_FIELD;
   } else if (ballDown >= HEIGHT_CANVAS) {
-    ball.setDirectionY(DIRECTION_UP);
-    ball.setDirectionX(DIRECTION_LEFT);
+    ball.setDirectionY(DIRECTION.UP);
+    ball.setDirectionX(DIRECTION.LEFT);
     ball.isFlying = false;
     ball.x = INITIAL_POSITION_BALL_X;
     ball.y = INITIAL_POSITION_BALL_Y;
@@ -341,7 +408,7 @@ function collisionBallAndPlatform(ball, platform) {
   const coordY = ball.y + ball.radius;
 
   if (coordX >= platform.x && coordX <= platform.x + LENGTH_PLATFORM && coordY >= INITIAL_POSITION_PLATFORM_Y && coordY < INITIAL_POSITION_PLATFORM_Y + platform.height) {
-    ball.setDirectionY(DIRECTION_UP);
+    ball.setDirectionY(DIRECTION.UP);
   }
 }
 
@@ -353,59 +420,59 @@ function collisionBallAndBrick(ball, brick) {
 
   if (!brick.isBroken) {
     if (coordX >= brick.x && coordXbelow <= brick.x + Brick.WIDTH) {
-      if (ball.directionX === DIRECTION_RIGHT) {
-        if (ball.directionY === DIRECTION_DOWN) {
+      if (ball.directionX === DIRECTION.RIGHT) {
+        if (ball.directionY === DIRECTION.DOWN) {
           if (coordY >= brick.y && coordY <= brick.y + 5 && coordX >= brick.x && coordX <= brick.x + 3) {
-            ball.setDirectionX(DIRECTION_LEFT);
-            ball.setDirectionY(DIRECTION_UP);
+            ball.setDirectionX(DIRECTION.LEFT);
+            ball.setDirectionY(DIRECTION.UP);
             brick.isBroken = true;
           } else if (coordY >= brick.y && coordY <= brick.y + 5) {
-            ball.setDirectionY(DIRECTION_UP);
+            ball.setDirectionY(DIRECTION.UP);
             brick.isBroken = true;
           } else if (ball.y > brick.y && ball.y <= brick.y + Brick.HEIGHT) {
-            ball.setDirectionX(DIRECTION_LEFT);
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionX(DIRECTION.LEFT);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           }
-        } else if (ball.directionY === DIRECTION_UP) {
+        } else if (ball.directionY === DIRECTION.UP) {
           if (coordYbelow >= brick.y && coordYbelow <= brick.y + 5 && coordX >= brick.x && coordX <= brick.x + 3) {
-            ball.setDirectionX(DIRECTION_LEFT);
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionX(DIRECTION.LEFT);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           } else if (coordYbelow >= brick.y && coordYbelow <= brick.y + 5) {
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           } else if (ball.y > brick.y && ball.y <= brick.y + Brick.HEIGHT) {
-            ball.setDirectionX(DIRECTION_RIGHT);
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionX(DIRECTION.RIGHT);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           }
         }
-      } else if (ball.directionX === DIRECTION_LEFT) {
-        if (ball.directionY === DIRECTION_DOWN) {
+      } else if (ball.directionX === DIRECTION.LEFT) {
+        if (ball.directionY === DIRECTION.DOWN) {
           if (coordY >= brick.y && coordY <= brick.y + 5 && coordXbelow >= brick.x && coordXbelow <= brick.x + 3) {
-            ball.setDirectionX(DIRECTION_RIGHT);
-            ball.setDirectionY(DIRECTION_UP);
+            ball.setDirectionX(DIRECTION.RIGHT);
+            ball.setDirectionY(DIRECTION.UP);
             brick.isBroken = true;
           } else if (coordY >= brick.y && coordY <= brick.y + 5) {
-            ball.setDirectionY(DIRECTION_UP);
+            ball.setDirectionY(DIRECTION.UP);
             brick.isBroken = true;
           } else if (ball.y > brick.y && ball.y <= brick.y + Brick.HEIGHT) {
-            ball.setDirectionX(DIRECTION_RIGHT);
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionX(DIRECTION.RIGHT);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           }
-        } else if (ball.directionY === DIRECTION_UP) {
+        } else if (ball.directionY === DIRECTION.UP) {
           if (coordYbelow >= brick.y && coordYbelow <= brick.y + 5 && coordX >= brick.x && coordX <= brick.x + 3) {
-            ball.setDirectionX(DIRECTION_RIGHT);
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionX(DIRECTION.RIGHT);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           } else if (coordYbelow >= brick.y && coordYbelow <= brick.y + 5) {
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           } else if (ball.y > brick.y && ball.y <= brick.y + Brick.HEIGHT) {
-            ball.setDirectionX(DIRECTION_LEFT);
-            ball.setDirectionY(DIRECTION_DOWN);
+            ball.setDirectionX(DIRECTION.LEFT);
+            ball.setDirectionY(DIRECTION.DOWN);
             brick.isBroken = true;
           }
         }
