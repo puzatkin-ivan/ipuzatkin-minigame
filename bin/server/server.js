cbcfe276d4e18c1fa9874afbd3ee0d564606cc5c
puzatkin-ivan <puzatkin1998@gmail.com>
2017-07-14 10:36:25 +0300

    Добавил статические файлы

--- bin/server/server.js
+++ bin/server/server.js
@@ -1,11 +1,17 @@
 var express = require('express');
-
 var app = express();
 
+const PORT = 8080;
+
+app.use('/static', express.static('../..' + '/css'));
+app.use('/static', express.static('../..' + '/image'));
+app.use('/static', express.static('..' + '/lib'));
+app.use('/static', express.static('../..' + '/src'));
+
 app.get('/', function(req, res) {
-  res.sendFile('/index.html', { root: "../" });
+  res.sendFile('/index.html', {root: '..' });
 });
 
-app.listen(8080);
+app.listen(PORT);
 
 console.log('Сервер стартовал!');
